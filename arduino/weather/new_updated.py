c037a221f64cf0a07c6d4141098bfc0623fc75f7
Rahul verma <100432854+NME-rahul@users.noreply.github.com>
2022-07-26 19:23:35 +0530

    Delete new_updated.py

--- arduino/weather/new_updated.py
+++
@@ -1,7 +0,0 @@
-import serial
-import time
-arduino = serial.Serial(port='COM3', baudrate=9600, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
-
-while(1):
-    print( arduino.readline())
-    time.sleep(0.5)
