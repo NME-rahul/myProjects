7ae98a847226efeab60ee94a54b69d25ffe904a8
Rahul verma <100432854+NME-rahul@users.noreply.github.com>
2022-07-27 19:41:44 +0530

    Update arduino-python.ino

--- weather/arduino-python/arduino-python.ino
+++ weather/arduino-python/arduino-python.ino
@@ -24,39 +24,37 @@ void create(float x){
   int r;
   int g;
   int b;
-  if( x>0 && x<5){r; g; b;}
-  else if( x>5 && x<8){r; g; b;}
-  else if( x>5 && x<8){r; g; b;}
-  else if( x>8 && x<10){r; g; b;}
-  else if( x>10 && x<13){r; g; b;}
-  else if( x>13 && x<15){r; g; b;}
-  else if( x>15 && x<18){r; g; b;}
-  else if( x>18 && x<20){r; g; b;}
-  else if( x>20 && x<22){r; g; b;}
-  else if( x>22 && x<24){r; g; b;}
-  else if( x>24 && x<27){r; g; b;}
-  else if( x>27 && x<30){r; g; b;}
-  else if( x>30 && x<32){r; g; b;}
-  else if( x>32 && x<34){r; g; b;}
-  else if( x>34 && x<36){r; g; b;}
-  else if( x>36 && x<38){r; g; b;}
-  else if( x>38 && x<40){r; g; b;}
-  else if( x>40 && x<41){r; g; b;}
-  else if( x>41 && x<42){r; g; b;}
-  else if( x>42 && x<43){r; g; b;}
-  else if( x>43 && x<44){r; g; b;}
-  else if( x>44 && x<45){r; g; b;}
-  else if( x>45 && x<46){r; g; b;}
-
+  
+  //0-20 is cold
+  if( x >= 0 && x <= 20 ){
+    r=0;  g=0; b=175;
+  }
+  
+  //20-30 is pleasent
+  else if( x > 20 && x <= 30 ){
+    r=118; g=215; b=0;
+  }
+  
+  //30-40 is hot and after it critical
+  else if( x > 30 && x <= 40 ){
+    r=175; g=143; b=41;
+  }
+  
+  //too hot
+  else if( x > 40 ){
+    r=175; g=41; b=41;
+  }
+ 
   for(int i=0; i<NUM_LEDS; i++){
-    leds[i] = CRGB(r, g, b);
+    leds[i] = CRGB(r+x, g+x, b+x);
     FastLED.show();
   }
 }
+
 void loop() {
     x = Serial.read();
     for(int i=0; i<50; i++){
         create(273.3-x);
-        delay(100);
+        delay(30);
     }
 }
