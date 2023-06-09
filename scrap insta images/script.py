83ec28e5efcfccba3a2ecef5d23f4b572e6ac423
Rahul verma <100432854+NME-rahul@users.noreply.github.com>
2023-06-10 02:15:55 +0530

    Update script.py

--- scrap insta images/script.py
+++ scrap insta images/script.py
@@ -5,29 +5,45 @@ import requests as rq
 from bs4 import BeautifulSoup
 
 def get_content(url):
-    return rq.get(str(url))
+    try:
+        res = rq.get(str(url), stream = True)
+    except Exception as error:
+        sys.exit(error)
+    return res
 
 def get_beautify(res):
     return BeautifulSoup(res, 'html.parser')
 
 def get_img_links(soup):
-    img_urls = []
+    img_urls = []; vid_urls = []
     for url in soup.find_all('img'):
         src = url.attrs['src']
         if 'data:image/png;base64' in src:
             pass
         else:
             img_urls.append(src)
+    for url in soup.find_all('video'):
+        src = url.attrs['src']
+        vid_urls.append(src)
 
-    return img_urls
+    return [img_urls, vid_urls]
 
 def download_images(urls, path, user):
     path = path + "\\insta_images\\"
     i = 0; os.system(f'mkdir {path}\\{user}')
-    for url in urls:
+    for url in urls[0]:
         try:
             with open(path + user + f'\\img{i}.jpeg', 'wb') as fp:
                 fp.write(get_content(url).content); i = i + 1
+        except Exception as error:
+            sys.exit(error)
+    for url in urls[1]:
+        urls = urls[5:]
+        try:
+            with open(path + user + f'\\vid{i}.mp4', 'wb') as fp:
+                for chunk in get_content(url).iter_content(chunk_size=1024*1024):
+                    if chunk:
+                        fp.write(chunk); i = i + 1
         except Exception as error:
             sys.exit(error)
 
@@ -56,6 +72,7 @@ if __name__ == '__main__':
         download_images(urls, os.getcwd(), user)
     else:
         res = get_content(sys.argv[1])
-        soup = get_beautify(res.content)
+        soup = get_beautify(res.contewnt)
         urls = get_img_links(soup)
-        download_images(urls, os.getcwd())
+        user = extract_info(soup)
+        download_images(urls, os.getcwd(), user)
